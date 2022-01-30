0b118871e3bcdab5f89948bc0e57c59d5ba006ab
lducoding <lducoding@gmail.com>
2022-01-30 20:37:55 +0900

    테스트 코드 작성법 공부

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -18,16 +18,22 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findById(Long id) {
+
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return store.values().stream().filter(member -> member.getId().equals(name)).findAny();
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
     }
 
     @Override
     public List<Member> findAll() {
+
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }

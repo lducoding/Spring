6faf258463622f73e24b79a0e5801c1387ed62bd
lducoding <lducoding@gmail.com>
2022-03-19 22:10:27 +0900

    springsecurity oauth2

---
+++ SpringSecurity-oauth2/src/main/java/com/cos/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.cos.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preauthorize 어노테이션 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hsaRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+//                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN") //이것도 가능
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+//                .usernameParameter() // 이걸 사용하면 사용자명을 form username 이외의 다른걸로 변경가능
+                .loginProcessingUrl("/login") //login 주소가 호출되면 스프리시큐리티 낚아채서 대신 로그인 진행한다
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm");
+    }
+}

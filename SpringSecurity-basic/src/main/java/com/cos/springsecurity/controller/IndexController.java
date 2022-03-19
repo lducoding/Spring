962b01affa0f2ea648bd6c9ffa22240922b20a71
lducoding <lducoding@gmail.com>
2022-03-19 22:09:55 +0900

    springSecurity basic

---
+++ SpringSecurity-basic/src/main/java/com/cos/springsecurity/controller/IndexController.java
@@ -0,0 +1,84 @@
+package com.cos.springsecurity.controller;
+
+import com.cos.springsecurity.model.User;
+import com.cos.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"","/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        if(user.getUsername().equals("user")) {
+            user.setRole("ROLE_USER");
+        } else if(user.getUsername().equals("manager")) {
+            user.setRole("ROLE_MANAGER");
+        } else if(user.getUsername().equals("admin")) {
+            user.setRole("ROLE_ADMIN");
+        }
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER')") //함수시작전에
+//    @PostAuthorize("hasRole('ROLE_MANAGER')") //끝난뒤
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "개인정보";
+    }
+}

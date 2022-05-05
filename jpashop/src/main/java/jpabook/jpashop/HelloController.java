87216bbb8adb9c09991c5d796349ac08f3301b44
lducoding <lducoding@gmail.com>
2022-05-05 20:41:16 +0900

    컨트롤러에서 호출

--- jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -1,15 +1,26 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HelloController {
+
+    private MemberRepository memberRepository;
 
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("test")
+    public List<Member> test(Model model) {
+        return memberRepository.findAll();
     }
 }

25f5bb3ed52a21c69f0604151c995f37adab98f7
lducoding <lducoding@gmail.com>
2022-05-14 23:00:53 +0900

    ItemServiceTest 생성:

---
+++ jpashop/src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemServiceTest {
+
+    @Test
+    public void itemSave() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void findItem() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void findOne() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+}

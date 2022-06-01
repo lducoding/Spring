6444d0c172556fe279ef44dd0407748690fa7d32
lducoding <lducoding@gmail.com>
2022-06-01 20:30:23 +0900

    jpa기본 동작방식

---
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        // jpa는 이 팩토리 기반으로 돌아가고 이건 db당 하나를 만들어야 함
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 고객의 요청이오면 이 매니저를 만들고 작업
+        EntityManager em = emf.createEntityManager();
+        // 트랜잭션 안에서 실행해야 한다.
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+//            Member findMember = em.find(Member.class, 2L);
+//            findMember.setName("없음");
+            List<Member> memberList = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            for (Member member : memberList) {
+                System.out.println("member.name:  " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            em.close();
+        }
+        emf.close();
+    }
+}

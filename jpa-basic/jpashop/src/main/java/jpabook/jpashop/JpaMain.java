b52c88bde11ce91c659134cec320ec32896687b6
lducoding <lducoding@gmail.com>
2022-06-05 02:13:03 +0900

    기본키 매핑 예제

---
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // jpa는 이 팩토리 기반으로 돌아가고 이건 db당 하나를 만들어야 함
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 고객의 요청이오면 이 매니저를 만들고 작업
+        EntityManager em = emf.createEntityManager();
+        // 트랜잭션 안에서 실행해야 한다.
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+
+            Order order = em.find(Order.class, 1L);
+            order.getMember().getId();
+
+            tx.commit();
+        } catch (Exception e) {
+            em.close();
+        }
+        emf.close();
+    }
+}

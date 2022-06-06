cbf22dcce2e23ef63e50cf7eb88e0b45ac2b2ecc
lducoding <lducoding@gmail.com>
2022-06-06 16:27:30 +0900

    연관관계 학습

--- jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -12,34 +12,7 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    //    @Column(name = "TEAM_ID")
-//    private Long teamId;
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void changeTeam(Team team) {
-        this.team = team;
-        team.getMembers().add(this);
-    }
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 }
